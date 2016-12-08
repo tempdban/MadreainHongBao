6db9c0755874a2a655c7d3f9ec6de3432c47d7e7
JackWu <965244491@qq.com>
2016-12-08 17:40:35 +0800

    add  project

---
+++ app/src/main/java/com/madreainhongbao/MadreainActivity.java
@@ -0,0 +1,18 @@
+package com.madreainhongbao;
+
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MadreainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+        startActivity(intent);
+    }
+}
